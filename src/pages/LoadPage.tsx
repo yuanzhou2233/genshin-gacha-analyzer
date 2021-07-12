4166fcb01ced193473e714dfc41f281a857cbc48
erinilis <389897773@qq.com>
2021-07-13 03:21:32 +0800

    update

--- src/pages/LoadPage.tsx
+++ src/pages/LoadPage.tsx
@@ -25,7 +25,7 @@ export const LoadPage: FC<LoadPageProps> = function ({ onLoad }) {
   const { updateParsedData, updatePage } = useGlobalContext();
   const params = qs.parse(document.location.search,{ignoreQueryPrefix: true})
   const uid = params.uid
-  const host = "http://erinilis.cn:7701"
+  const host = "https://erinilis.cn:7701"
   // const host = "http://127.0.0.1:7701"
   if (uid){
     fetch(`${host}/genshin/gachalog/xlsx/${uid}`)
